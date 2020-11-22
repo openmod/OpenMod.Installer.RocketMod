5c60db8929ef348082b02d1bd7d0bf3e341fdb89
Enes Sadık Özbek <es.ozbek@outlook.com>
2020-11-23 00:51:22 +0300

    Progress

--- src/OpenModInstallerPlugin.cs
+++ src/OpenModInstallerPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenMod.Installer.RocketMod.Jobs;
 using Rocket.Core.Plugins;
 
 namespace OpenMod.Installer.RocketMod
@@ -6,10 +7,18 @@ namespace OpenMod.Installer.RocketMod
     public class OpenModInstallerPlugin : RocketPlugin
     {
         public static OpenModInstallerPlugin Instance { get; private set; }
+        public OpenModManager OpenModManager { get; private set; }
+        public JobsManager JobsManager { get; private set; }
 
         protected override void Load()
         {
             Instance = this;
+            
+            OpenModManager = new OpenModManager("todo: find or create openmod directory");
+            JobsManager = new JobsManager();
+
+            // JobsManager.RegisterJob(new OpenModInstallJob());
+
             base.Load();
         }
 
---
+++ src/OpenModManager.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenMod.Installer.RocketMod
+{
+    public class OpenModManager
+    {
+        public OpenModManager(string workingDirectory)
+        {
+            WorkingDirectory = workingDirectory;
+        }
+
+        public string WorkingDirectory { get; } // gets the openmod directory
+    }
+}
