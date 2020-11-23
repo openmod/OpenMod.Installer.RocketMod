6f93a8d09fca5dbcc84d2a2348b6c4e00415befc
Charterino <charterinod@gmail.com>
2020-11-23 15:30:55 +0300

    Added Migrate and Revert to JobsManager

--- src/Jobs/JobsManager.cs
+++ src/Jobs/JobsManager.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rocket.Core.Logging;
 
 namespace OpenMod.Installer.RocketMod.Jobs
 {
@@ -19,6 +22,78 @@ namespace OpenMod.Installer.RocketMod.Jobs
         public IReadOnlyCollection<IJob> GetJobs()
         {
             return _jobs;
+        }
+
+        public void Migrate(string[] preventionFlags = null)
+        {
+            for (int i = 0; i < _jobs.Count; i++)
+            {
+                var job = _jobs.ElementAt(i);
+                
+                var jobName = job.GetType().Name;
+                if (preventionFlags != null)
+                {
+                    if (job.GetType().HasPreventAttribute())
+                    {
+                        var preventionFlag = job.GetType().GetPreventionCommand();
+                        if (preventionFlags.Contains(preventionFlag))
+                        {
+                            Logger.Log($"Recognized {preventionFlag}, preventing {jobName}");
+                            continue;
+                        }
+                    }
+                }
+                Logger.Log($"Executing {jobName}");
+                try
+                {
+                    job.ExecuteMigration();
+                }
+                catch (Exception e)
+                {
+                    Logger.Log($"Caught exception while executing {jobName}:");
+                    Logger.LogException(e);
+                    Logger.Log("Reverting all possible jobs.");
+                    Revert(i, preventionFlags);
+                    return;
+                }
+            }
+
+            Logger.Log("Migration successfully finished. You can restart your server now.");
+        }
+
+        private void Revert(int stoppedAt, string[] preventionFlags = null)
+        {
+            for (int i = stoppedAt; i >= 0; i--)
+            {
+                var job = _jobs.ElementAt(i);
+                if (job is IRevertable revertableJob)
+                {
+                    var jobName = revertableJob.GetType().Name;
+                    if (preventionFlags != null)
+                    {
+                        if (job.GetType().HasPreventAttribute())
+                        {
+                            var preventionFlag = job.GetType().GetPreventionCommand();
+                            if (preventionFlags.Contains(preventionFlag))
+                            {
+                                Logger.Log($"Recognized {preventionFlag}, preventing reverting of {jobName} because it never happened.");
+                                continue;
+                            }
+                        }
+                    }
+                    Logger.Log($"Reverting {jobName}");
+                    try
+                    {
+                        revertableJob.Revert();
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Log($"Caught exception while reverting {jobName}:");
+                        Logger.LogException(e);
+                    }
+                }
+            }
+            Logger.Log("OpenMod installation has failed. No changes were made. Please report this issue on the OpenMod discord: https://discord.gg/J9KYMaMaTN");
         }
     }
 }
