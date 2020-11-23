7e9a4068c7f4f405f52957a57140eec0c4692b9a
Charterino <charterinod@gmail.com>
2020-11-23 13:03:55 +0300

    Added PreventAttribute

---
+++ src/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using OpenMod.Installer.RocketMod.Jobs;
+
+namespace OpenMod.Installer.RocketMod
+{
+    public static class Extensions
+    {
+        public static bool HasPreventAttribute(this Type type)
+        {
+            return type.GetCustomAttribute(typeof(PreventAttribute)) != null;
+        }
+
+        public static string GetPreventionCommand(this Type type)
+        {
+            if (!type.HasPreventAttribute())
+                throw new ArgumentException();
+            var castedAttribute = (PreventAttribute) type.GetCustomAttribute(typeof(PreventAttribute));
+            return castedAttribute._commandToPrevent;
+        }
+    }
+}
---
+++ src/Jobs/PreventAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenMod.Installer.RocketMod.Jobs
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PreventAttribute : Attribute
+    {
+        public PreventAttribute(string command)
+        {
+            _commandToPrevent = command;
+        }
+
+        public readonly string _commandToPrevent;
+    }
+}
