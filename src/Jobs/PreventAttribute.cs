d438b6512f2ec28f2e51db91a33ee3a59b1acd0a
Stephen White <iamsilk3495@gmail.com>
2020-11-24 23:07:38 -0400

    Added allowance of multiple PreventAttributes

--- src/Jobs/PreventAttribute.cs
+++ src/Jobs/PreventAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace OpenMod.Installer.RocketMod.Jobs
 {
-    [AttributeUsage(AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class PreventAttribute : Attribute
     {
         public PreventAttribute(string command)
