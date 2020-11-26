739959b7d42938d9bc550ba1ba722830f0cf3adb
Charterino <charterinod@gmail.com>
2020-11-26 14:51:12 +0300

    Introduced OpenModRocketModUninstallJob

---
+++ src/Jobs/OpenModRocketModUninstallJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMod.Installer.RocketMod.Jobs
+{
+    public class OpenModRocketModUninstallJob : IJob, IRevertable
+    {
+        public void ExecuteMigration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Revert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
