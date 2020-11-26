58d3f014af8211ec9e3c6fac1256839c602c08f1
Charterino <charterinod@gmail.com>
2020-11-26 14:51:39 +0300

    Added AssetName property to Asset

--- src/Models/Asset.cs
+++ src/Models/Asset.cs
@@ -4,6 +4,9 @@ namespace OpenMod.Installer.RocketMod.Models
 {
     public class Asset
     {
+        [JsonProperty("name")]
+        public string AssetName { get; set; }
+
         [JsonProperty("browser_download_url")]
         public string BrowserDownloadUrl { get; set; }
     }
