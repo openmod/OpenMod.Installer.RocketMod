a6fcc8a983977ce8861f61234942e0947497a407
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-11-27 16:55:27 +0700

    Add helpers

---
+++ src/Helpers/AsyncHelper.cs
@@ -0,0 +1,14 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenMod.Installer.RocketMod.Helpers
+{
+    public static class AsyncHelper
+    {
+        public static void RunSync(Func<Task> func)
+        {
+            AsyncContext.Run(func);
+        }
+    }
+}
