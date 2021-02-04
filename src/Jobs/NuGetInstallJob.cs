76ac249cd6ed090660be551cd72885b44c6e5348
Enes Sadık Özbek <es.ozbek@outlook.com>
2021-02-04 18:18:58 +0300

    Improve nuget install messages

--- src/Jobs/NuGetInstallJob.cs
+++ src/Jobs/NuGetInstallJob.cs
@@ -19,50 +19,56 @@ namespace OpenMod.Installer.RocketMod.Jobs
 
         public void ExecuteMigration()
         {
-            Logger.Log($"Installing package \"{m_PackageId}\"...");
             AsyncHelperEx.RunSync(DownloadPackage);
-            Logger.Log($"Installed package \"{m_PackageId}\".");
         }
 
         private async Task DownloadPackage()
         {
+            Logger.Log($"Installing package \"{m_PackageId}\"...");
             var nuGetPackageManager = NuGetHelper.GetNuGetPackageManager();
 
             const bool c_AllowPreReleaseVersion = false;
 
-            var OldPackageidentity = await nuGetPackageManager.GetLatestPackageIdentityAsync(m_PackageId);
-            if (OldPackageidentity == null)
+            var oldIdentity = await nuGetPackageManager.GetLatestPackageIdentityAsync(m_PackageId);
+            if (oldIdentity != null)
             {
-                var package = await nuGetPackageManager.QueryPackageExactAsync(m_PackageId, null, c_AllowPreReleaseVersion);
-                if (package == null || package.Identity == null)
-                {
-                    Logger.Log($"Downloading has failed for {m_PackageId}: {NuGetInstallCode.PackageOrVersionNotFound}");
-                    return;
-                }
-                var identity = package.Identity;
-
-                var installResult = await nuGetPackageManager.InstallAsync(identity, c_AllowPreReleaseVersion);
-                bool isInstalled;
-                if (Enum.GetValues(typeof(NuGetInstallCode)).Length == 3) // loaded 3.0 version openmod 
-                {
-                    isInstalled = (int)installResult.Code is 0 or 1; // Success / NoUpdatesFound
-                }
-                else
-                {
-                    isInstalled = installResult.Code == NuGetInstallCode.Success;
-                }
-
-                if (isInstalled)
-                {
-                    m_PackageDirectory = Path.Combine(OpenModInstallerPlugin.Instance.OpenModManager.WorkingDirectory, "packages",
-                        identity.ToString());
-                    Logger.Log($"Finished downloading \"{m_PackageId}\".");
-                }
-                else
-                {
-                    Logger.Log($"Downloading has failed for {m_PackageId}: {installResult.Code}");
-                }
+                Logger.Log($"Package \"{m_PackageId}\" is already installed.");
+                return;
             }
+
+            var package = await nuGetPackageManager.QueryPackageExactAsync(m_PackageId, null, c_AllowPreReleaseVersion);
+            if (package?.Identity == null)
+            {
+                Logger.Log($"Downloading has failed for {m_PackageId}: {NuGetInstallCode.PackageOrVersionNotFound}");
+                return;
+            }
+
+            var identity = package.Identity;
+
+            var installResult = await nuGetPackageManager.InstallAsync(identity, c_AllowPreReleaseVersion);
+            bool isInstalled;
+            if (Enum.GetValues(typeof(NuGetInstallCode)).Length == 3) // loaded 3.0 version openmod 
+            {
+                isInstalled = (int) installResult.Code is 0 or 1; // Success / NoUpdatesFound
+            }
+            else
+            {
+                isInstalled = installResult.Code == NuGetInstallCode.Success;
+            }
+
+            if (isInstalled)
+            {
+                m_PackageDirectory = Path.Combine(OpenModInstallerPlugin.Instance.OpenModManager.WorkingDirectory,
+                    "packages",
+                    identity.ToString());
+                Logger.Log($"Finished downloading \"{m_PackageId}\".");
+            }
+            else
+            {
+                Logger.Log($"Downloading has failed for {m_PackageId}: {installResult.Code}");
+            }
+
+            Logger.Log($"Installed package \"{m_PackageId}\"@{identity.Version}.");
         }
 
         public void Revert()
